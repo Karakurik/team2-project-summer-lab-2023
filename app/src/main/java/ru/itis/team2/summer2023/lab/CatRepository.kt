40ec4ab95d9c7d8531452a2878a3d9305b62432a
ngurevaa <nngurevaa@gmail.com>
2023-07-05 20:21:44 +0300

    catalog and actions

---
+++ app/src/main/java/ru/itis/team2/summer2023/lab/CatRepository.kt
@@ -0,0 +1,9 @@
+package ru.itis.team2.summer2023.lab
+
+object CatRepository {
+    val list: List<Cat> = listOf(
+        Cat(1, "уличная", 1, R.raw.cat, true),
+        Cat(2, "hdhvhbhjvb.knb", 2, R.raw.cat, false),
+        Cat(3, "нвсв",3, R.raw.cat, false)
+    )
+}
--- app/src/main/java/ru/itis/team2/summer2023/lab/start/CatalogFragment.kt
+++ app/src/main/java/ru/itis/team2/summer2023/lab/start/CatalogFragment.kt
@@ -1,18 +1,57 @@
 package ru.itis.team2.summer2023.lab.start
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.dialog.MaterialDialogs
+import dev.shreyaspatil.MaterialDialog.MaterialDialog
+import ru.itis.team2.summer2023.lab.CatAdapter
+import ru.itis.team2.summer2023.lab.CatRepository
 import ru.itis.team2.summer2023.lab.R
 import ru.itis.team2.summer2023.lab.databinding.FragmentCatalogBinding
+import ru.itis.team2.summer2023.lab.game.GameActivity
 
 class CatalogFragment : Fragment(R.layout.fragment_catalog) {
     private var binding: FragmentCatalogBinding? = null
+    private var adapter: CatAdapter? = null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentCatalogBinding.bind(view)
-    }
 
+        initAdapter()
+
+    }
+    private fun initAdapter() {
+        adapter = CatAdapter(CatRepository.list, Glide.with(this)
+        ) { cat ->
+            if (cat.open) {
+                val intent = Intent(requireContext(), GameActivity::class.java)
+                startActivity(intent)
+            }
+            else {
+                AlertDialog.Builder(activity)
+                    .setTitle("Хотите забрать кота к себе?")
+                    .setNegativeButton("Нет") { dialog, which -> }
+                    .setPositiveButton("Да") { dialog, which ->
+                        // if (очков заботы хватает) {
+                        // списание очков заботы
+                        // переход к данному коту
+                        val intent = Intent(requireContext(), GameActivity::class.java)
+                        startActivity(intent)
+                        // иначе сообщение об ошибке можно сделать snackbar
+                        // или можно сделать ещё один alertdialog
+                    }
+                    .show()
+            }
+        }
+        binding?.rvCat?.adapter = adapter
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
