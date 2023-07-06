1b40e84695a6cb55f15021c545ca4b8083247f58
ngurevaa <nngurevaa@gmail.com>
2023-07-06 15:47:20 +0300

    catalog and actions

--- app/src/main/java/ru/itis/team2/summer2023/lab/Cat.kt
+++ app/src/main/java/ru/itis/team2/summer2023/lab/Cat.kt
@@ -2,8 +2,12 @@ package ru.itis.team2.summer2023.lab
 
 data class Cat(
     val id: Int,
-    val breed: String,
+    val breed: Int,
     val carePoints: Int,
     val urlImage: Int,
-    val open: Boolean
+    val open: Boolean,
+    val hunger: Int,
+    val happy: Int,
+    val sleep: Int,
+    val purity: Int
 )
